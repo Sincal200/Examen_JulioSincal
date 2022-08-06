4eb5b5ee21490684bbf813495df99c32a892e340
Sincal <sincaljulio200@gmail.com>
2022-08-06 11:59:54 -0600

    Creación Empleado y Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente extends Persona{
+
+  private String nombre_empresa;
+  private int telefono_de_contacto;
+
+  public void mostrar(){
+    System.out.println("");
+
+  }
+
+
+}
---
+++ src/Empleado.java
@@ -0,0 +1,13 @@
+public class Empleado extends Persona {
+    private double sueldo_bruto;
+
+    public void mostrar(){
+        System.out.println("");
+    }
+
+    public double calcular_salario_neto(){
+        return sueldo_bruto;
+
+    }
+}
+
